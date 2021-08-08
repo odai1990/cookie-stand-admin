574803303a8042d3ba52d60ced41779adf5ac054
odai1990 <odai22odai78@gmail.com>
2021-08-08 13:14:49 +0300

    done

--- components/loginform.js
+++ components/loginform.js
@@ -22,7 +22,7 @@ function Loginform(props) {
     return(
 
         <>
-          <form className="flex flex-col w-1/2 gap-4 p-8 mx-auto my-4 text-center bg-gray-200 border-2 border-gray-400 rounded-lg text-md" onSubmit={submitHandler}>
+          <form className="flex flex-col w-1/2 gap-4 p-8 mx-auto my-4 text-center bg-green-200 border-2 border-green-400 rounded-lg text-md" onSubmit={submitHandler}>
             <div className="flex flex-col ">
                 <label className="mb-2 font-bold uppercase text-grey-darkest" htmlFor="username">User Name</label>
                 <input className="px-3 py-2 border text-grey-darkest" type="text" name="username" id="username" placeholder="User Name" onChange={usernameChangeHandler} />
@@ -35,7 +35,7 @@ function Loginform(props) {
                 <input className="px-3 py-2 border text-grey-darkest" type="password" name="password" id="password" placeholder="password" onChange={passwordChangeHandler}/>
             </div>
 
-            <button className="px-3 py-4 mt-8 uppercase bg-gray-500 rounded text-green hover:bg-gray-600 text-gray-50" type="submit">Sign In</button>
+            <button className="px-3 py-4 mt-8 uppercase bg-green-500 rounded text-green hover:bg-green-600 text-green-50" type="submit">Sign In</button>
 
         </form>
         </>
